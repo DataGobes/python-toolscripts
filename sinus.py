2748a9628bd6ac0d597be9276fcf5454fb3918c4
DataGobes <gcc.jansen@gmail.com>
2017-05-06 08:07:13 +0200

    user input added

--- sinus.py
+++ sinus.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([np.sin(x) for x in np.arange(start=-5, stop=5, step=0.01)], label='sinus')
-plt.plot([np.cos(x) for x in np.arange(start=-5, stop=5, step=0.01)], label='cosinus')
+start = int(input('Where to start plotting ?:'))
+stop = int(input('Where to stop plotting? :'))
+
+plt.plot([np.sin(x) for x in np.arange(start=start, stop=stop, step=0.01)], label='sinus')
+plt.plot([np.cos(x) for x in np.arange(start=start, stop=stop, step=0.01)], label='cosinus')
 plt.xlabel('0.01 increments')
 plt.ylabel('value')
 plt.legend()
