9c9032bb046995322e07e7e1bb2dafae2a8fee2b
DataGobes <gcc.jansen@gmail.com>
2017-05-06 08:00:47 +0200

    evo1

--- cpu.py
+++ cpu.py
@@ -1,6 +1,8 @@
 import time
 import psutil
+import numpy as np
 
 while True:
-    print(psutil.cpu_percent(percpu=True))
+    a = psutil.cpu_percent(percpu=True)
+    print('CPU percentages :'+str(a) + ' Mean :' + str(np.around(np.mean(a),decimals=2)))
     time.sleep(1)
--- sinus.py
+++ sinus.py
@@ -3,5 +3,7 @@ import matplotlib.pyplot as plt
 
 plt.plot([np.sin(x) for x in np.arange(start=-5, stop=5, step=0.01)], label='sinus')
 plt.plot([np.cos(x) for x in np.arange(start=-5, stop=5, step=0.01)], label='cosinus')
+plt.xlabel('0.01 increments')
+plt.ylabel('value')
 plt.legend()
 plt.show()
