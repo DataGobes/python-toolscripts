a24239c2753c16c4fbd48a71420cb0329bf29f6c
DataGobes <gcc.jansen@gmail.com>
2017-05-06 08:15:44 +0200

    test1

--- sinus.py
+++ sinus.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-start = int(input('Where to start plotting ?:'))
-stop = int(input('Where to stop plotting? :'))
+sta = int(input('Where to start plotting ?:'))
+sto = int(input('Where to stop plotting? :'))
 
-plt.plot([np.sin(x) for x in np.arange(start=start, stop=stop, step=0.01)], label='sinus')
-plt.plot([np.cos(x) for x in np.arange(start=start, stop=stop, step=0.01)], label='cosinus')
+plt.plot([np.sin(x) for x in np.arange(start=sta, stop=sto, step=0.01)], label='sinus')
+plt.plot([np.cos(x) for x in np.arange(start=sta, stop=sto, step=0.01)], label='cosinus')
 plt.xlabel('0.01 increments')
 plt.ylabel('value')
 plt.legend()
